88098e447f34906e8c6557c357a0419a2918dc07
Glitch (low-harmonious-walnut) <none>
2020-05-03 21:46:31 +0000

    👌💂 Test Error

--- api/controller/book.controller.js
+++ api/controller/book.controller.js
@@ -4,6 +4,12 @@ var controllers = {}
 controllers.index = async function(req, res, next) {
   var books = await Book.find();
   res.json(books)
+  try {
+    var a; 
+    a.b();
+  } catch {
+    res.render('error/error500')
+  }
 }
 
 module.exports = controllers
--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -3,7 +3,9 @@ var controllers = {}
 
 controllers.index = async function(req, res, next) {
   var users = await User.find();
+  
   res.json(users)
+
 }
 
 module.exports = controllers
